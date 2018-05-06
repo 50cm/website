0aec93af5055f6d860cd3f6f4cbc6604629b79dc
shundroid <shundroidk@gmail.com>
2018-05-06 16:32:30 +0900

    updated header

---
+++ src/components/CHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <c-title />
+  </header>
+</template>
+
+<script>
+import CTitle from './CTitle'
+export default {
+  components: {
+    CTitle
+  }
+}
+</script>
+
+<style scoped>
+header {
+  width: 100vw;
+  height: 100vh;
+  background-image: url('../assets/back.jpg');
+  background-size: cover;
+  background-position: center;
+  padding: 30vh 30vw 0 0;
+  box-sizing: border-box;
+  text-align: right;
+}
+</style>
---
+++ src/components/CTitle.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{count}}cm</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  mounted() {
+    this.increment()
+  },
+  methods: {
+    increment() {
+      this.count++
+      if (this.count < 50) {
+        setTimeout(this.increment, 20)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  margin: 0;
+  color: white;
+  font-size: 5vw;
+}
+</style>
